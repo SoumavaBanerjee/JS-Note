7d736ec6524cff31240a1077129bd5a17727e950
SoumavaBanerjee <soumava.rivu@gmail.com>
2021-03-29 22:01:07 +0530

    implemented css bundling. code refactor

--- src/plugins/unpkgFetchPackagePlugin.ts
+++ src/plugins/unpkgFetchPackagePlugin.ts
@@ -12,22 +12,56 @@ export const unpkgFetchPackagePlugin = (inputString: string) => {
   return {
     name: "unpkg-Fetch-Package-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: onLoadInterface) => {
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputString,
-          };
-        }
+      build.onLoad({ filter: /(^index\.js$)/ }, (args: onLoadInterface) => {
+        return {
+          loader: "jsx",
+          contents: inputString,
+        };
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: onLoadInterface) => {
+        const cachedPackage = await packageCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        if (cachedPackage) return cachedPackage;
+
+        const { data, request } = await axios.get(args.path);
+        console.log(args.path);
+
+        // Escape newLines(\n), single-quotes('') and doubleQuotes("") and comments(/**/, // ) in fetched css
+        const escapedCssString = data
+          .replace(/\n/g, "")
+          .replace(/\/\*[\s\S]*?\*\//g, "")
+          .replace(/"/g, `\\"`)
+          .replace(/'/g, `\\'`);
+
+        const contents = `
+          const styleTag = document.createElement('style');
+          styleTag.innerText = '${escapedCssString}';
+          document.head.appendChild(styleTag);
+        `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        // set new result in cache
+        packageCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: onLoadInterface) => {
         // check cache for file path
         const cachedPackage = await packageCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
-        // if found return
         if (cachedPackage) return cachedPackage;
 
         const { data, request } = await axios.get(args.path);
+        console.log(args.path);
 
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
