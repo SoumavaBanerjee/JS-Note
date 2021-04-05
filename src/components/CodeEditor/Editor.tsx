5dd42bcec95fc3428eb8de63d135d1578da6733a
SoumavaBanerjee <soumava.rivu@gmail.com>
2021-04-05 22:58:50 +0530

    prettier code format in monaco editor done

--- src/components/CodeEditor/Editor.tsx
+++ src/components/CodeEditor/Editor.tsx
@@ -1,14 +1,51 @@
-import MonacoEditor from "@monaco-editor/react";
+import MonacoEditor, { Monaco } from "@monaco-editor/react";
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import { EditorPropsInterface } from "../../interfaces/index";
-import React from "react";
+import React, {useRef} from "react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
 
 const Editor: React.FC<EditorPropsInterface> = ({ onChange, initialValue }) => {
-  const onEditorDidMount = (getCurrentValue: any) => {
-    console.log(getCurrentValue);
+
+
+  const monacoEditorRef = useRef<monaco.editor.IStandaloneCodeEditor>();
+
+
+  /**
+   * On change in model, get the value of it's content and
+   * pass it as an argument to onChange to set the input state
+   */
+  const onEditorDidMount = (
+    editor: monaco.editor.IStandaloneCodeEditor,
+    monacoInstance: Monaco
+  ) => {
+    // console.log(editor.getValue());
+    monacoEditorRef.current = editor;
+    editor.onDidChangeModelContent(() => {
+      onChange(editor.getValue());
+    });
   };
 
+  /**
+   * Get unformatted code, format it and set it to model
+   */
+  const onFormatClick = () => {
+    const rawCode = monacoEditorRef!.current!.getModel()!.getValue();
+    const formattedCode = prettier.format(rawCode, {
+      parser: "babel",
+      plugins:[parser],
+      semi: true,
+      singleQuote: true
+    })
+
+    monacoEditorRef.current?.setValue(formattedCode);
+  }
+
+
   return (
-    <MonacoEditor
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
       onMount={onEditorDidMount}
       value={initialValue}
       height="30vh"
@@ -27,6 +64,8 @@ const Editor: React.FC<EditorPropsInterface> = ({ onChange, initialValue }) => {
         automaticLayout: true,
       }}
     />
+    </div>
+    
   );
 };
 
